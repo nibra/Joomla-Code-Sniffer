52b740f5203e98bd52cb0f830cc8181e2c49e667
Niels Braczek <nbraczek@bsds.de>
2011-08-11 05:20:37 +0200

    Made Copyright Sniff work

--- Sniffs/Commenting/FileCommentSniff.php
+++ Sniffs/Commenting/FileCommentSniff.php
@@ -673,16 +673,16 @@ class Joomla_Sniffs_Commenting_FileCommentSniff implements PHP_CodeSniffer_Sniff
             $content = $copyright->getContent();
             if ($content !== '') {
                 $matches = array();
-                if (preg_match('/^.*?([0-9]{4})((.{1})([0-9]{4}))? (.+)$/', $content, $matches) !== 0) {
+                if (preg_match('~^.*?(\d{4})(?:(.)(\d{4}))? (.+)$~', $content, $matches) !== 0) {
                     // Check earliest-latest year order.
-                    if ($matches[3] !== '') {
-                        if ($matches[3] !== '-') {
+                    if ($matches[2] > '') {
+                        if ($matches[2] != '-') {
                             $error = 'A hyphen must be used between the earliest and latest year';
                             $this->currentFile->addError($error, $errorPos, 'CopyrightHyphen');
                         }
 
-                        if ($matches[4] !== '' && $matches[4] < $matches[1]) {
-                            $error = "Invalid year span \"$matches[1]$matches[3]$matches[4]\" found; consider \"$matches[4]-$matches[1]\" instead";
+                        if (0 < $matches[3] && $matches[3] < $matches[1]) {
+                            $error = "Invalid year span \"$matches[1]$matches[2]$matches[3]\" found; consider \"$matches[3]-$matches[1]\" instead";
                             $this->currentFile->addWarning($error, $errorPos, 'InvalidCopyright');
                         }
                     }
